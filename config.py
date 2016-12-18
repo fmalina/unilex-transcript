e1a57c1bca163dd8d91290633d3ecf0f005a7e35
Jacob Wahlgren <jacob.wahlgren@gmail.com>
2016-12-18 20:57:09 +0100

    Improve config.py
    * Make clear that the provided paths are placeholders.
    * Use os.path.join to make more foolproof (handles /path as well as
      /path/) and is cross platform.
    * Comment out example REMOVE_BEFORE et al because they are not generally
      useful defaults.

--- config.py
+++ config.py
@@ -1,25 +1,34 @@
+from os import path
+
 # This is your project root, configure your own path.
-DATA_DIR = '/Users/f/DATA/OFSTED/'
+DATA_DIR = '/path/to/your/data/dir/'
 
 # PDF folder within your project root. PDFs to convert come from here.
-PDF_DIR  = DATA_DIR+'PDF'
-# HTML folder is where pdf2htmlEX outputs (non-semantic HTML) after running `./pdf2html.py`.
-HTML_DIR = DATA_DIR+'HTML'
-# used by ttf.py to access full original fonts to compare with the broken ones
-FULL_FONTS_PATH  = '/Users/f/SITES/etc/ttf'
-# remove mumbo-jumbo TEXT strings before HTML processing (REGEXes or text)
+PDF_DIR  = path.join(DATA_DIR, 'PDF')
+
+# HTML folder is where pdf2htmlEX outputs (non-semantic HTML) after running
+# `./pdf2html.py`.
+HTML_DIR = path.join(DATA_DIR, 'HTML')
+
+# Used by ttf.py to access full original fonts to compare with the broken ones,
+# e.g. /Users/f/SITES/etc/ttf or /usr/share/fonts or ~/.fonts.
+FULL_FONTS_PATH  = '/path/to/truetype/fonts/'
+
+# Remove mumbo-jumbo text strings before HTML processing (REGEXes or text)
 REMOVE_BEFORE = (
-    r'The Office for Standards.*?www\.ofsted\.gov\.uk',
-    r'Any complaints.*?\@ofsted\.gov\.uk',
-    r'© Crown copyright 20\d\d',
-    r'Inspection grades:.*?inspection terms',
-    r'This letter.*?their school\.',
-    r'You can use Parent View.*?www\.ofsted\.gov\.uk'
+    # r'The Office for Standards.*?www\.ofsted\.gov\.uk',
+    # r'Any complaints.*?\@ofsted\.gov\.uk',
+    # r'© Crown copyright 20\d\d',
+    # r'Inspection grades:.*?inspection terms',
+    # r'This letter.*?their school\.',
+    # r'You can use Parent View.*?www\.ofsted\.gov\.uk'
 )
-# find and replace after HTML processing finished
+
+# Find and replace after HTML processing is finished.
 REPLACE_AFTER = (
-    (r'td>( )?Overall effectiveness j', 'td colspan=4>Overall effectiveness j'),
+    # (r'td>( )?Overall effectiveness j', 'td colspan=4>Overall effectiveness j'),
 )
+
 # Additional bullet point characters to be expected at start of line for <li>
 # Copied out of the processsed PDF. Common bullets are pre-programmed.
 BULLETS =  ('', '')
